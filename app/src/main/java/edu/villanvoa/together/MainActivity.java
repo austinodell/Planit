ef746e5898a1f9d06e56c2437d72e290cd6a3e57
arinald <arinald@gmail.com>
2015-02-10 13:01:40 -0500

    Added Facebook Login
    Added Facebook login top blank project.  When user is logged in it
    automatically opens up another activity.

--- app/src/main/java/edu/villanvoa/together/MainActivity.java
+++ app/src/main/java/edu/villanvoa/together/MainActivity.java
@@ -1,39 +1,37 @@
 package edu.villanvoa.together;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
+
+    private static final String TAG = "MainFragment";
+
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        setContentView(R.layout.fragment_login);
+        Log.d(TAG, "onCreate");
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, loginFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            loginFragment = (LoginFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
