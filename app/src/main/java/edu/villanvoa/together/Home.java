ee8fbddaefdb875bdd6aa667575fdf2e66767583
Austin Odell <austin@odell.cc>
2015-03-16 13:09:11 -0400

    Use new Event Object

--- app/src/main/java/edu/villanvoa/together/Home.java
+++ app/src/main/java/edu/villanvoa/together/Home.java
@@ -2,37 +2,23 @@ package edu.villanvoa.together;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Outline;
 import android.graphics.Point;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 
 
-public class Home extends ActionBarActivity {
+public class Home extends ToolbarActivity {
 
-    private Display display;
-    private Point size;
-    public static int screenWidth;
-    public int screenHeight;
     private ArrayList<Event> eventsList;
     private Context mContext;
+
+    private int event_id = 0; // temporary - to be populated by Parse
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,32 +27,32 @@ public class Home extends ActionBarActivity {
 
         mContext = this;
 
-        /* Set up toolbar to replace Actionbar */
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar mSupportActionBar = getSupportActionBar();
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toolbar.setTitle(R.string.title_activity_home);
+        setupToolbar(R.string.title_activity_home);
 
+        // Initializes Universal Image Loader Library to load images given URLs
+        ImageLib imgLib = new ImageLib(mContext);
+
+        // Used to store all events to pass to HomeGridAdapter
         eventsList = new ArrayList<Event>();
 
-        addEvent("Movie Night",R.drawable.movie);
-        addEvent("Bowling",R.drawable.bowl);
-        addEvent("Passion Pit Show",R.drawable.contert_big);
-        addEvent("Restaurant Week",R.drawable.dinner);
-        addEvent("Bar Hop",R.drawable.bar);
-        addEvent("Club Shampoo",R.drawable.club);
-        addEvent("KOP",R.drawable.shop);
-        addEvent("S'mores",R.drawable.smores);
-        addEvent("Picnic on Sheehan Beach",R.drawable.picnic);
-        addEvent("Game at Wells Fargo",R.drawable.basketball);
-        addEvent("Blue Mountain",R.drawable.ski);
-        addEvent("Road Trip to Boston",R.drawable.roadtrip);
+        addEvent(this.event_id++,"Movie Night","http://images.clipartpanda.com/movie-night-clipart-9cp4q9xcE.jpeg"); // Example with image to fetch
+        addEvent(this.event_id++,"Bowling",R.drawable.bowl); // Examples with image to use drawable ids
+        addEvent(this.event_id++,"Passion Pit Show",R.drawable.contert_big);
+        addEvent(this.event_id++,"Restaurant Week",R.drawable.dinner);
+        addEvent(this.event_id++,"Bar Hop",R.drawable.bar);
+        addEvent(this.event_id++,"Club Shampoo",R.drawable.club);
+        addEvent(this.event_id++,"KOP",R.drawable.shop);
+        addEvent(this.event_id++,"S'mores",R.drawable.smores);
+        addEvent(this.event_id++,"Picnic on Sheehan Beach",R.drawable.picnic);
+        addEvent(this.event_id++,"Game at Wells Fargo",R.drawable.basketball);
+        addEvent(this.event_id++,"Blue Mountain",R.drawable.ski);
+        addEvent(this.event_id++,"Road Trip to Boston",R.drawable.roadtrip);
 
+        // Setup Adapter
         GridView gridView = (GridView) findViewById(R.id.container);
-        gridView.setAdapter(new HomeGridAdapter(this,eventsList));
+        gridView.setAdapter(new HomeGridAdapter(this,eventsList,imgLib));
 
+        // Add New Event Button Setup
         Button new_event_btn = (Button) findViewById(R.id.new_event_btn);
         new_event_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,18 +62,15 @@ public class Home extends ActionBarActivity {
         });
     }
 
-    private void addEvent(String name, int img_resid) {
-        Event event = new Event(name,img_resid);
+    // Add event with Image URL
+    private void addEvent(int id, String name, String img_url) {
+        Event event = new Event(id,name,img_url);
         eventsList.add(event);
     }
 
-    public static class Event {
-        public final String name;
-        public final int drawableId;
-
-        Event(String name, int drawableId) {
-            this.name = name;
-            this.drawableId = drawableId;
-        }
+    // Add event with Image Drawable Resource (preloaded)
+    private void addEvent(int id, String name, int img_resid) {
+        Event event = new Event(id,name,img_resid);
+        eventsList.add(event);
     }
 }
